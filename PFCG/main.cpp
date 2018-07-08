e59b464fc3d91c1ff171c0a1a6a6eeba1e311288
liellison <liellisonjoseph@gmail.com>
2018-07-08 11:03:37 -0300

    Criacao das paredes externas do labirinto em 2D

--- PFCG/main.cpp
+++ PFCG/main.cpp
@@ -13,14 +13,12 @@
 
 void display(void);
 void reshape(int, int);
+GLfloat xf, yf, win;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitWindowSize(512, 512);
-    
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-    
     glutCreateWindow("Labirinto Magico de Liellison");
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
@@ -30,26 +28,55 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void display()
-{
+void display(){
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f);
     
-    glColor3f(1, 0, 0);
-    
-    glBegin(GL_TRIANGLES);
-    
-    glVertex2f(10, 10);
-    glVertex2f(250, 400);
-    glVertex2f(400, 10);
-    
+    //Parede de entrada
+    glBegin(GL_QUADS);
+        glVertex2i(0, 50);
+        glVertex2i(0, 30);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(384,30);
+        glVertex2i(384,50);
     glEnd();
-    
-    glutSwapBuffers();
+    glBegin(GL_QUADS);
+        glVertex2i(404, 50);
+        glVertex2i(404, 30);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(512,30);
+        glVertex2i(512,50);
+    glEnd();
+    //Parede esquerda
+    glBegin(GL_QUADS);
+        glVertex2i(0, 512);
+        glVertex2i(0, 50);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(20,50);
+        glVertex2i(20,512);
+    glEnd();
+    //Parede direita
+    glBegin(GL_QUADS);
+        glVertex2i(492, 512);
+        glVertex2i(492, 50);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(512,50);
+        glVertex2i(5122,512);
+    glEnd();
+    //Parede de cima
+    glBegin(GL_QUADS);
+        glVertex2i(0, 512);
+        glVertex2i(0, 492);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2i(512,492);
+        glVertex2i(512,512);
+    glEnd();
+    glFlush();
 }
 
-void reshape(int width, int height)
-{
-    
+void reshape(int width, int height){
     glViewport(0,0,width,height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
