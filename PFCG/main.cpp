69316135b90a11162fb214ba530e63e7d02d7576
liellison <liellisonjoseph@gmail.com>
2018-07-07 22:07:58 -0300

    Commit inical

---
+++ PFCG/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PFCG
+//
+//  Created by Liellison on 07/07/18.
+//  Copyright © 2018 Liellison. All rights reserved.
+//
+
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
